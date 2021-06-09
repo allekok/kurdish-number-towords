8b89f0b5cbe0ba54ce1e3eceaa6bee664fb0d566
Payam <payambapiri.97@gmail.com>
2021-06-09 19:58:37 +0430

    'kurdish-number-to-words.js' -> 'num_to_str.js' + Trying to simplify things.

---
+++ num_to_str.js
@@ -0,0 +1,41 @@
+const D = {
+	to_20: ['سفر', 'یەک', 'دوو', 'سێ', 'چوار', 'پێنج', 'شەش', 'حەوت',
+		'هەشت', 'نۆ', 'دە', 'یازدە', 'دوازدە', 'سێزدە', 'چاردە', 'پازدە',
+		'شازدە', 'حەڤدە', 'هەژدە', 'نۆزدە'],
+	to_100: ['بیست', 'سی', 'چل', 'پەنجا', 'شەست', 'حەفتا', 'هەشتا', 'نەوەد'],
+	1e2: ' سەد',
+	1e3: ' هەزار',
+	1e6: ' ملیۆن',
+	1e9: ' ملیارد',
+	1e12: ' تریلیۆن',
+	neg: 'مەنفی ',
+	and: ' و '
+}
+function num_to_str(n, l=0) {
+	const i = parseInt
+	let R = ''
+	
+	if(n == 0)
+		return l == 0 ? D.to_20[0] : ''
+	if(n < 0)
+		return D.neg + num_to_str(-n, l)
+	if(l > 0) {
+		R += D.and
+		l--
+	}
+	if(n < 20)
+		R += D.to_20[n]
+	else if(n < 1e2)
+		R += D.to_100[i(n / 10) - 2] + num_to_str(n % 10, l + 1)
+	else if(n < 1e3)
+		R += num_to_str(i(n/1e2),l) + D[1e2] + num_to_str(n%1e2,l+1)
+	else if(n < 1e15) {
+		for(let j = 1e3; j < 1e15; j *= 1e3)
+			if(n < (j * 1e3)) {
+				R += num_to_str(i(n / j), l) + D[j] +
+					num_to_str(n % j, l + 1)
+				break
+			}
+	}
+	return R
+}
